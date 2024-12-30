759e620be927bc33d4bec780e6dbec5aa239a973
ibimina hart <ibimina.c.hart@gmail.com>
2024-12-30 23:29:04 +0100

    python naming conversion

---
+++ variables.py
@@ -0,0 +1,26 @@
+# Variables can
+
+# starts with letters
+name = "mina"
+
+# include alphanumeric characters
+name0001 = "mina"
+
+# begin with an underscore
+__name__ = ""
+
+
+# can't
+# include exclamation mark  "name!"
+# include hyphen with variable name "first-name" 
+# start with a numeric character  "1name"
+# use reserved keywords e.g if, for w.t.c
+
+
+# Examples of Expression
+2 + 2
+"mina"
+
+
+# Example of Statement
+myname = "Mina"
